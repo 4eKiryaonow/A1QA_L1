1ff1bb9ad6a5fda676096452785509c982f3f9ab
Kirill Popov <k.popov@qa-academy.by>
2023-02-15 20:17:41 +0300

    add 3rd test

--- src/test/java/task1/DataProviderTest.java
+++ src/test/java/task1/DataProviderTest.java
@@ -3,6 +3,7 @@ package task1;
 import entity.User;
 import org.testng.annotations.DataProvider;
 import reader.JsonReader;
+import reader.YamlReader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,17 +12,47 @@ public class DataProviderTest {
 
 
     @DataProvider(name = "set1")
-    public static Object[][] getDataForTest() {
-        List<User> listData1 = new ArrayList<>(JsonReader.getDataFromJsonFile(
-                "src/test/resources/admin_users.json"));
-        List<User> listData2 = new ArrayList<>(JsonReader.getDataFromJsonFile(
-                "src/test/resources/regular_users.json"));
-
+    public static Object[][] getDataForTest1() {
+        List<User> listData1 = new ArrayList<>(
+                JsonReader.getDataFromJsonFile("src/test/resources/admin_users.json"));
+        List<User> listData2 = new ArrayList<>(
+                JsonReader.getDataFromJsonFile("src/test/resources/regular_users.json"));
 
         return new Object[][] {
                 {listData1},
                 {listData2}
         };
+    }
+
+    @DataProvider(name = "set2")
+    public static Object[][] getDataForTest2() {
+        List<User> listData1 = new ArrayList<>(
+                YamlReader.getDataFromYamlFile("src/test/resources/admin_users.yaml"));
+        List<User> listData2 = new ArrayList<>(
+                YamlReader.getDataFromYamlFile("src/test/resources/regular_users.yaml"));
+
+        return new Object[][] {
+                {listData1},
+                {listData2}
+        };
+    }
+
+    @DataProvider(name = "set3")
+    public static Object[][] getDataForTest3() {
+        List<User> listJson1 = new ArrayList<>(
+                JsonReader.getDataFromJsonFile("src/test/resources/admin_users.json"));
+        List<User> listJson2 = new ArrayList<>(
+                JsonReader.getDataFromJsonFile("src/test/resources/regular_users.json"));
+        List<User> listYaml1 = new ArrayList<>(
+                YamlReader.getDataFromYamlFile("src/test/resources/admin_users.yaml"));
+        List<User> listYaml2 = new ArrayList<>(
+                YamlReader.getDataFromYamlFile("src/test/resources/regular_users.yaml"));
+
+        return new Object[][] {
+                {listJson1, listYaml1},
+                {listJson2, listYaml2}
+        };
+
     }
 }
 
--- src/test/java/task1/TestClass.java
+++ src/test/java/task1/TestClass.java
@@ -3,17 +3,33 @@ package task1;
 import entity.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestClass extends BaseTest {
 
 
     @Test(dataProvider = "set1", dataProviderClass = DataProviderTest.class)
-    public void validateJsonFileContainsListOfUsers(ArrayList<User> list) {
+    public void checkJsonFileContainsListOfUsers(ArrayList<User> list) {
 
         Assert.assertEquals(list.size(), validateServiceClass.checkUserHaveIdAndName(list),
                 "Json file consists users which has no ID or Name");
+
+    }
+
+    @Test(dataProvider = "set2", dataProviderClass = DataProviderTest.class)
+    public void checkYamlFileContainsListOfUser(ArrayList<User> list) {
+
+        Assert.assertEquals(list.size(), validateServiceClass.checkUserHaveIdAndName(list),
+                "Yaml file consists users which has no ID or Name");
+    }
+
+    @Test(dataProvider = "set3", dataProviderClass = DataProviderTest.class)
+    public void checkJsonAllUsersContainsInYaml(List<User> jsonList, List<User> yamlList) {
+
+        Assert.assertTrue(yamlList.containsAll(jsonList));
 
 
     }
