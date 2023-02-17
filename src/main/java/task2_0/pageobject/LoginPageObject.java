25ba57fad5a761a65c6375afc5103581b84e0d06
Kirill Popov <k.popov@qa-academy.by>
2023-02-18 01:26:55 +0300

    LiginPageObject class is added

---
+++ src/main/java/task2_0/pageobject/LoginPageObject.java
@@ -0,0 +1,33 @@
+package task2_0.pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPageObject extends AbstractPageObject {
+
+    @FindBy(xpath = "//input[@type= 'text']")
+    private WebElement loginField;
+    @FindBy(xpath = "//input[@type= 'password']")
+    private WebElement passwordField;
+    @FindBy(xpath = "//button[@type= 'submit']")
+    private WebElement submitButton;
+    @FindBy(xpath = "//button[contains(@class, 'SubmitButton')]/descendant::div[contains(@class, 'LoadingSpinner')]")
+    private WebElement iconLoading;
+
+    public LoginPageObject(WebDriver driver) {
+        super(driver);
+    }
+public void login(String login, String password) {
+        wait.until(ExpectedConditions.visibilityOf(loginField)).sendKeys(login);
+        wait.until(ExpectedConditions.visibilityOf(passwordField)).sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(submitButton)).click();
+}
+
+public WebElement getIconLoading() {
+
+        return iconLoading;
+}
+
+}
--- src/test/java/task2_0/InvalidLoginTestClass.java
+++ src/test/java/task2_0/InvalidLoginTestClass.java
@@ -2,6 +2,7 @@ package task2_0;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import task2_0.pageobject.LoginPageObject;
 import task2_0.pageobject.MainPageObject;
 
 public class InvalidLoginTestClass extends SeleniumBaseClass {
@@ -11,6 +12,11 @@ public class InvalidLoginTestClass extends SeleniumBaseClass {
         MainPageObject mainPageObject = new MainPageObject(driver);
         Assert.assertEquals(mainPageObject.getUrl(), "https://store.steampowered.com/");
         mainPageObject.header().clickLoginButton();
+
+        LoginPageObject loginPageObject = new LoginPageObject(driver);
+        loginPageObject.login("sdfsdfsdf", "sdfsdfsdf");
+        Assert.assertTrue(loginPageObject.getIconLoading().isDisplayed(), "Loading element is not displayed");
+
 
 
 
