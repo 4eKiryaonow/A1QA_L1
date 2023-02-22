b30459066c72c0e5ab191fbcd31e4765491899e4
Kirill Popov <k.popov@qa-academy.by>
2023-02-22 17:21:27 +0300

    add Exception class

---
+++ src/main/java/task3_1/exception/IllegalBrowserNameException.java
@@ -0,0 +1,18 @@
+package task3_1.exception;
+
+import task3_1.driver.Browser;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class IllegalBrowserNameException extends IllegalArgumentException {
+
+    public IllegalBrowserNameException() {
+        super(String.format("Unsupported browser name. Supported browsers are %s",
+                Arrays.stream(Browser.values())
+                        .map(browser -> browser.name().toLowerCase(Locale.ROOT))
+                        .collect(Collectors.joining(", "))
+        ));
+    }
+}
