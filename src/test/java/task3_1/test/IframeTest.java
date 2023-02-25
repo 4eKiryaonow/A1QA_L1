e870516160546326a4ef22de0504d707941da701
Kirill Popov <k.popov@qa-academy.by>
2023-02-25 09:30:59 +0300

    Second step of test was added

--- src/test/java/task3_1/test/IframeTest.java
+++ src/test/java/task3_1/test/IframeTest.java
@@ -1,16 +1,25 @@
 package task3_1.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import task3_1.pageobject.AlertsFrameWindowsPageObject;
 import task3_1.pageobject.MainPageObject;
+import task3_1.pageobject.NestedFramesPageObject;
 
 public class IframeTest {
 
     @Test
     public void iFrameTest() {
 
-        System.out.println("Step 2. Click  on \"Alerts, Frame & Windows\" button. In a menu click \"Nested Frames\" button");
         MainPageObject mainPageObject = new MainPageObject();
-        mainPageObject.clickAlertsFrameWindowsBtn();
+        Assert.assertTrue(mainPageObject.formIsOpened());
+
+        System.out.println("Step 2. Click  on \"Alerts, Frame & Windows\" button. In a menu click \"Nested Frames\" button");
+        AlertsFrameWindowsPageObject alertsFrameWindowsPageObject = mainPageObject.clickAlertsFrameWindowsBtn();
+        NestedFramesPageObject nestedFramesPageObject = alertsFrameWindowsPageObject.LeftPanel().clickNestedFrames();
+        Assert.assertTrue(nestedFramesPageObject.formIsOpened(), "Page \"Nested Frames\" hasn't been opened");
+        //add assert about messages Parent frame and Children frame
+
 
     }
 }
