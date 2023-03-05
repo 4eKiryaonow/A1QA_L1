aec2a0e6fe81200150593b2d63e19f6c0bbb3ea4
Kirill Popov <k.popov@qa-academy.by>
2023-03-05 11:38:06 +0300

    SamplePage PO was created

---
+++ src/test/java/task3_1/pageobject/SamplePageObject.java
@@ -0,0 +1,16 @@
+package task3_1.pageobject;
+
+import org.openqa.selenium.By;
+import task3_1.base.BaseForm;
+import task3_1.element.Label;
+
+public class SamplePageObject extends BaseForm {
+
+    private static Label labelSamplePage = new Label(By.id("sampleHeading"), "SamplePage Label");
+
+    public SamplePageObject() {
+        super(labelSamplePage, "SamplePage");
+    }
+    
+
+}
