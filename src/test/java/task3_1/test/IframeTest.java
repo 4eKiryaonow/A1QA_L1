c17e83da1b4f239a800b6cc30e6c78a73641dd13
Kirill Popov <k.popov@qa-academy.by>
2023-03-06 10:50:05 +0300

    logs were added for IframeTest

--- src/test/java/task3_1/test/IframeTest.java
+++ src/test/java/task3_1/test/IframeTest.java
@@ -10,17 +10,20 @@ import task3_1.pageobject.component.BottomFrame;
 import task3_1.pageobject.component.ParentFrameComponent;
 import task3_1.pageobject.component.TopFrame;
 import task3_1.util.TestDataManager;
+import task3_1.utils.InfoLogger;
 
 public class IframeTest extends BaseTestClass {
 
     @Test(testName = "Iframe",
             dataProvider = "Iframe test", dataProviderClass = TestDataProvider.class)
     public void iFrameTest(TestDataManager data) {
+        InfoLogger.info("Test \"Iframe\" starts");
 
+        InfoLogger.info("Step 1. Navigate to Main Page");
         MainPageObject mainPageObject = new MainPageObject();
         Assert.assertTrue(mainPageObject.formIsOpened(), "\"Main page\" hasn't been opened");
 
-        System.out.println("Step 2. Click  on \"Alerts, Frame & Windows\" button. In a menu click \"Nested Frames\" button");
+        InfoLogger.info("Step 2. Click  on \"Alerts, Frame & Windows\" button. In a menu click \"Nested Frames\" button");
         mainPageObject.clickAlertsFrameWindowsBtn();
         AlertsFrameWindowsPageObject alertsFrameWindowsPageObject = new AlertsFrameWindowsPageObject();
         alertsFrameWindowsPageObject.LeftPanel().clickNestedFrames();
@@ -32,7 +35,8 @@ public class IframeTest extends BaseTestClass {
         parentFrameComponent.childFrame().switchToChildFrame();
         Assert.assertEquals(parentFrameComponent.getChildFrameText(), data.getChildFrameText(), "Text on the child frame doesn't match");
         parentFrameComponent.leaveFrame();
-        System.out.println("Step 3. Select \"Frames\" option in a left menu");
+
+        InfoLogger.info("Step 3. Select \"Frames\" option in a left menu");
         nestedFramesPageObject.leftPanel().clickFramesBtn();
         FramesPageObject framesPageObject = new FramesPageObject();
         Assert.assertTrue(framesPageObject.formIsOpened(), "Page \"Frames\" hasn't been opened");
@@ -45,9 +49,7 @@ public class IframeTest extends BaseTestClass {
         String bottomFrameText = bottomFrame.getBottomFrameText();
         Assert.assertTrue(topFrameText.equals(bottomFrameText), "Text is not equals for both frames");
 
-
-
-
+        InfoLogger.info("Test \"Iframe\" is finished.");
 
     }
 }
