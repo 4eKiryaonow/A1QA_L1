b46c550123509dbd3305d19c4b9c54c98eddd1ba
Kirill Popov <k.popov@qa-academy.by>
2023-03-07 09:46:31 +0300

    BaseFrame is refactored as a sepearate entity

--- src/main/java/task3_1/base/BaseFrame.java
+++ src/main/java/task3_1/base/BaseFrame.java
@@ -4,23 +4,32 @@ import org.openqa.selenium.By;
 import task3_1.utils.FrameUtil;
 import task3_1.utils.InfoLogger;
 
-public abstract class BaseFrame extends BaseForm {
+public abstract class BaseFrame {
 
     private By frameLocator;
+    private BaseElement baseElement;
+    private String formName;
 
     public BaseFrame(BaseElement element, By frameLocator, String formName) {
-        super(element, formName);
+
+        this.baseElement = element;
         this.frameLocator = frameLocator;
+        this.formName = formName;
     }
 
     public void switchToFrame() {
-        InfoLogger.info(String.format("switchToFrame() for: %s",this.getFormName()));
+        InfoLogger.info(String.format("switchToFrame() for: %s",this.getFrameName()));
         FrameUtil.switchToFrame(frameLocator);
     }
 
     public void leaveFrame() {
-        InfoLogger.info(String.format("leaveFrame() for: %s", this.getFormName()));
+        InfoLogger.info(String.format("leaveFrame() for: %s", this.getFrameName()));
         FrameUtil.switchBackFromFrame();
+    }
+
+    public String getFrameName() {
+
+        return formName;
     }
 
 }
