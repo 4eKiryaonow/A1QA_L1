3c4e5be2123f11c091b16c8bfd13425a1aa64e60
Kirill Popov <k.popov@qa-academy.by>
2023-03-14 14:15:29 +0300

    improvements for ConfigReader

--- src/main/java/task3_1/utils/ConfigReader.java
+++ src/main/java/task3_1/utils/ConfigReader.java
@@ -6,11 +6,17 @@ import task3_1.models.Config;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.PipedReader;
 import java.util.List;
 
 public class ConfigReader {
 
     private static JSONParser reader = new JSONParser();
+    private static String BROWSER;
+    private static List<String> OPTIONS;
+    private static String URL;
+    private static String DURATION;
+
 
     public static Config readConfig(Config config, String path) {
 
@@ -25,10 +31,17 @@ public class ConfigReader {
             InfoLogger.error(e.getMessage());
             throw new RuntimeException(e);
         }
-        config.setBrowser((String) jsonObject.get(Constants.BROWSER_KEY));
-        config.setOptions((List<String>) jsonObject.get(Constants.OPTIONS_KEY));
-        config.setUrl((String) jsonObject.get(Constants.URL_KEY));
-        config.setWaitDuration((String) jsonObject.get(Constants.DURATION_KEY));
+
+        BROWSER = (String) jsonObject.get(Constants.BROWSER_KEY);
+        OPTIONS = (List<String>) jsonObject.get(Constants.OPTIONS_KEY);
+        URL = (String) jsonObject.get(Constants.URL_KEY);
+        DURATION = (String) jsonObject.get(Constants.DURATION_KEY);
+
+
+        config.setBrowser(BROWSER);
+        config.setOptions(OPTIONS);
+        config.setUrl(URL);
+        config.setWaitDuration(DURATION);
 
         return config;
     }
